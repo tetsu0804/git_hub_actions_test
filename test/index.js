410d50852086395a16f029d40fb4723b69ba5c0a
tetsu0804 <banginhoodyonezawa@gmail.com>
2022-02-01 23:38:15 +0900

    わざと test/index.jsを失敗させてみる

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ const fizzbuzz = require('../index.js')
 
 describe('FizzBuzzTest', () => {
   it('15を渡すと FizzBuzz', () => {
-    assert(fizzbuzz(15) === 'FizzBuzz');
+    assert(fizzbuzz(15) === 15);
   });
 
   it('1+1 = 2', () => {
